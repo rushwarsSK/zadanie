8ba89e0a43576b4e28385a3efa34cc37ca94309a
ZimanErik <erik.ziman@zoznam.sk>
2023-12-10 15:18:46 +0100

    added function for adding two integers together

--- mywork.c
+++ mywork.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int addition(int a, int b){
 
+    return a+b;
+
+}
 int main() {
-    printf("nieco ine");
+    int first_digit=3;
+    int second_digit=5;
+    printf("Eriks function returned: %d\n",addition(first_digit,second_digit));
     return 0;
 }
